de8fc45271b772dc52d3ed46fc654d97083a02ef
Olivier <olivierhoogers@gmail.com>
2023-06-27 09:17:52 +0200

    player fix (wrong branch) but easiest fix.

--- Monogame_Herkansing/Player.cs
+++ Monogame_Herkansing/Player.cs
@@ -12,7 +12,7 @@ namespace Monogame_Herkansing
     internal class Player
     {
         private Texture2D _playerTexture;
-        private Vector2 _position; 
+        private Vector2 _position;
         private float _speed;
         private bool isPressed;
 
@@ -28,20 +28,23 @@ namespace Monogame_Herkansing
             _position = position;
             _speed = speed;
         }
-        public void Update(GameTime gameTime)
+        public void Update(GameTime gameTime, int screenWidth, int screenHeight)
         {
-            // Get the current keyboard state
             KeyboardState keyboardState = Keyboard.GetState();
 
             // Update player movement based on keyboard input
-            if (keyboardState.IsKeyDown(Keys.W))
+            if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
                 _position.Y -= _speed;
-            if (keyboardState.IsKeyDown(Keys.S))
+            if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
                 _position.Y += _speed;
-            if (keyboardState.IsKeyDown(Keys.A))
+            if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
                 _position.X -= _speed;
-            if (keyboardState.IsKeyDown(Keys.D))
+            if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
                 _position.X += _speed;
+
+            // Restrict player movement within the screen boundaries
+            _position.X = MathHelper.Clamp(_position.X, 0, screenWidth - 130);
+            _position.Y = MathHelper.Clamp(_position.Y, 0, screenHeight - 120);
 
             // Check if spacebar is pressed and create new bullet if it is not already pressed.
             if (keyboardState.IsKeyDown(Keys.Space))
@@ -54,14 +57,16 @@ namespace Monogame_Herkansing
             }
             else
             {
-                _speed = _speed;
                 isPressed = false;
             }
         }
 
+
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_playerTexture, _position, Color.White);
+            float scale = 0.33f; // 50% scaling factor
+
+            spriteBatch.Draw(_playerTexture, _position, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
         }
     }
 
